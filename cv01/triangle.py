b28a051839718d6453d40a0ff2ae943f239dd50d
Petr (Laptop) <petrbohac3@seznam.cz>
2023-03-06 13:31:29 +0100

    production ready

--- cv01/triangle.py
+++ cv01/triangle.py
@@ -11,5 +11,15 @@ def triangle(a, b, c):
 
     Pro jednoduchost můžete předpokládat, že strany a, b jsou odvěsny, c je přepona.
     Tak jako je to ve známé matematické poučce.
+
+    Pavel Vacha made me do all these production ready checks.
     """
+
+    """Nula or None check"""
+    if not a or not b or not c:
+        return False
+
+    if a < 0 or b < 0 or c < 0:
+        return False
+
     return a ** 2 + b ** 2 == c ** 2
