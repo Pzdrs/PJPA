0a00e3d89748f79aa10f210bef0fe647b82a7afa
Petr (Desktop) <petrbohac3@seznam.cz>
2023-04-24 12:23:44 +0200

    vysosany data z html done

--- cv07/relay.py
+++ cv07/relay.py
@@ -15,6 +15,13 @@ Kompletní zadání je jako vždy na https://elearning.tul.cz/
 
 """
 import json
+import re
+
+from bs4 import BeautifulSoup
+
+RELAY_RESULTS_PATTERN = re.compile(
+    r'\d+\)\s(\w+\s*\w*)\s(\d+:\d+:\d+)\s\(([a-zA-Z- ]+),\s([a-zA-Z- ]+),\s([a-zA-Z- ]+)\)'
+)
 
 
 def output_json(result_list):
@@ -26,5 +33,18 @@ def output_json(result_list):
         output.write(json.dumps(result_list, indent=4, sort_keys=True))
 
 
+def extract_relay_data(html_file: str):
+    """
+    Extrahuje data ze souboru s výsledky štafetového závodu.
+    """
+    with open(html_file, 'r', encoding='utf-8') as html:
+        soup = BeautifulSoup(html, 'html.parser')
+        paragraphs = soup.findAll('p')
+        women = paragraphs[18]
+        men = paragraphs[20]
+        return tuple(RELAY_RESULTS_PATTERN.findall(women.text)), \
+            tuple(RELAY_RESULTS_PATTERN.findall(men.text))
+
+
 if __name__ == '__main__':
-    pass
+    relay_data = extract_relay_data('result.html')
